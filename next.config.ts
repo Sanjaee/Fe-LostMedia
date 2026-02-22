ef61ea412760c72de8695f5c94edb3d8ded9c09c
Sanjaee <afrizaahmad18@gmail.com>
2026-02-22 17:20:45 +0700

    Update Next.js configuration for standalone output and image domains
    - Changed output setting to "standalone" for optimized deployment.
    - Updated image domains to include new sources and removed outdated ones, enhancing image loading capabilities.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
   reactCompiler: true,
   reactStrictMode: true,
   images: {
@@ -9,12 +9,12 @@ const nextConfig: NextConfig = {
       "res.cloudinary.com",
       "i.pinimg.com",
       "via.placeholder.com",
-      "axiomtrading.sfo3.cdn.digitaloceanspaces.com",
       "localhost",
-      "localhost:5000",
+      "lostmedia.zacloth.com",
+      "zacloth.com",
       "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com", // Google profile pictures
-      "googleusercontent.com", // Other Google image domains
+      "lh3.googleusercontent.com",
+      "googleusercontent.com",
     ],
   },
 };
